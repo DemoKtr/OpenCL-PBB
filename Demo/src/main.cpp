e172be24134c0458133ad9a3a61fbc68cc8aa795
Piotr Mroczek <80900800+DemoKtr@users.noreply.github.com>
2024-09-01 12:39:52 +0200

    fast fixess

--- Demo/src/main.cpp
+++ Demo/src/main.cpp
@@ -8,6 +8,6 @@ int main() {
 
     toon_postprocess("swords.jpg","edges_detection_output.jpg");
     pbb_postprocess("swords.jpg", "PBB_output.jpg");
-    mosaic_effect("swords.jpg", "Mosaic_output.jpg",5,30); //third argument should be x%10==0 and must be x%2==0
+    mosaic_effect("swords.jpg", "Mosaic_output.jpg",5,30); //third argument should be x%10==0 and must be 
     return 0;
 }
